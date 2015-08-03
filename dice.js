a530e2561bb3c4f668956fb2b46c260e24dc3176
Klint Tiley <kctiley@gmail.com>
2015-08-03 13:49:33 -0600

    Added multiple dice roll

--- dice.js
+++ dice.js
@@ -1,11 +1,24 @@
+// var dice = {
+//   rolled: null,
+//   diceRoll: function(){
+//     this.rolled = Math.floor(Math.random() * 6)
+//   }
+// }
+
+// dice.diceRoll();
+// console.log(dice.rolled)
+
+
 var dice = {
-  rolled: null,
-  diceRoll: function(){
-    this.rolled = Math.floor(Math.random() * 6)
-  }
-}
+  rolled: [],
+   diceRoll: function(numberOfDice){
+    var newArray = [];
+    for(var i=0; i<numberOfDice; i++){
+      newArray.push(Math.floor(Math.random() * 6));
+    }
+    this.rolled = newArray;
+   }
+ }
 
-dice.diceRoll();
-console.log(dice.rolled)
-
-
+ dice.diceRoll(3);
+ console.log(dice.rolled)
