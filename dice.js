a68adc68e8c2daca661e8e5539d629ddbc210f13
Klint Tiley <kctiley@gmail.com>
2015-08-03 14:21:07 -0600

    Added constructor for dice.

--- dice.js
+++ dice.js
@@ -1,7 +1,7 @@
 // var dice = {
 //   rolled: null,
 //   diceRoll: function(){
-//     this.rolled = Math.floor(Math.random() * 6)
+//     this.rolled = Math.floor(Math.random() * 6+1)
 //   }
 // }
 
@@ -14,7 +14,7 @@
 //    diceRoll: function(numberOfDice){
 //     var newArray = [];
 //     for(var i=0; i<numberOfDice; i++){
-//       newArray.push(Math.floor(Math.random() * 6));
+//       newArray.push(Math.floor(Math.random() * 6+1));
 //     }
 //     this.rolled = newArray;
 //    }
@@ -24,16 +24,28 @@
 //  console.log(dice.rolled)
 
 
-var dice = {
-  rolled: [],
-   diceRoll: function(numberOfDice, sidesOfDice){
-    var newArray = [];
-    for(var i=0; i<numberOfDice; i++){
-      newArray.push(Math.floor(Math.random() * sidesOfDice + 1));
-    }
-    this.rolled = newArray;
-   }
- }
+// var dice = {
+//   rolled: [],
+//    diceRoll: function(numberOfDice, sidesOfDice){
+//     var newArray = [];
+//     for(var i=0; i<numberOfDice; i++){
+//       newArray.push(Math.floor(Math.random() * sidesOfDice + 1));
+//     }
+//     this.rolled = newArray;
+//    }
+//  }
 
- dice.diceRoll(3, 8);
- console.log(dice.rolled)
+//  dice.diceRoll(3, 8);
+//  console.log(dice.rolled)
+
+
+function Dice(){
+  this.value = null;
+  this.rollDice = function(){
+    this.value = Math.floor(Math.random() * 6+1)
+  };
+}
+
+var dice1 = new Dice();
+dice1.rollDice();
+console.log(dice1.value);
