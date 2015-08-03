08c6f73344e823fc614e8e1138be082b717fb156
Klint Tiley <kctiley@gmail.com>
2015-08-03 14:27:23 -0600

    Added constructor for multiple dice.

--- dice.js
+++ dice.js
@@ -38,14 +38,38 @@
 //  dice.diceRoll(3, 8);
 //  console.log(dice.rolled)
 
+// Using Constructor
+// function Dice(){
+//   this.value = null;
+//   this.rollDice = function(){
+//     this.value = Math.floor(Math.random() * 6+1)
+//   };
+// }
+
+// var dice1 = new Dice();
+// dice1.rollDice();
+// console.log(dice1.value);
+
 
 function Dice(){
-  this.value = null;
-  this.rollDice = function(){
-    this.value = Math.floor(Math.random() * 6+1)
-  };
+  this.value = [];
+  this.rollDice = function(numberOfDice){
+    var newArray = [];
+    for(var i=0; i<numberOfDice; i++){
+      newArray.push(Math.floor(Math.random() * 6+1));
+    };
+    this.value = newArray;
+   };
 }
 
 var dice1 = new Dice();
-dice1.rollDice();
+dice1.rollDice(15);
 console.log(dice1.value);
+
+
+
+
+
+
+
+
